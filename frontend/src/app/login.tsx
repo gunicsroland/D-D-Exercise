49891c72254461787a561248216897677371ce5f
Gunics Roland <grolcsi09@gmail.com>
2025-11-09 12:28:09 +0100

    fix: forgot to comming login.tsx

--- frontend/src/app/login.tsx
+++ frontend/src/app/login.tsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import {ScrollView, Button} from 'react-native';
+import { useState, useEffect } from 'react';
+import {ScrollView, Button, Text, TextInput, Alert} from 'react-native';
 import FormBox from '../components/form_box';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 
 
-const Login = () => {
+export default function Login () {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token")
+      if (token){
+        router.replace("/index")
+      }
+    }
+    checkToken();
+  });
+
+  const handleLogin = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({username, password})
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        await AsyncStorage.setItem("token", data.access_token);
+        router.replace("/index")
+      }
+      else {
+        const data = await res.json();
+        Alert.alert("Hiba", data.detail || "Hibás adatok");
+      }
+    }
+    catch(err){
+      Alert.alert("Hiba", "Nem sikerült kapcsolódni a szerverhez. Próbáld újra később");
+    }
+  }
+
   return (
-    <ScrollView>
-      <FormBox text_value="Felhasználónév" placeholder="Add meg a felhasználóneved" />
-      <FormBox text_value="Jelszó" placeholder="Add meg a jelszavad" />
-      <Button title="back" onPress={() => router.back()} />
+    <ScrollView style={styles.container}>
+      <Text>Bejelentkezés</Text>
+      <TextInput placeholder="Felhasználónév" value={username} onChangeText={setUsername} />
+      <TextInput placeholder="Jelszó" value={password} onChangeText={setPassword} secureTextEntry />
+      <Button title="Bejelentkezés" onPress={handleLogin} />
+      <Button title="Regisztráció" onPress={() => router.push("/register")} />
     </ScrollView>
   );
 };
@@ -27,5 +66,3 @@ const styles = {
     borderWidth: 1,
   },
 };
-
-export default Login;
