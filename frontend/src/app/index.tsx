2fff03881a77c66538de41e24d1f5e9d1d30a60b
Gunics Roland <grolcsi09@gmail.com>
2026-02-07 12:03:33 +0100

    chore(frontend): Remove AuthContext and useAuth hooks

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -3,22 +3,18 @@ import { View, Text, Button, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import { checkCharacter } from "../hooks/check_char";
-import { useAuth } from "../hooks/useAuth";
 
 
 export default function Home() {
-  const { user, setUser } = useAuth();
   const router = useRouter();
 
   const handleLogout = async () => {
     await AsyncStorage.removeItem("token");
-    setUser(null);
     router.replace("login");
   };
 
   return (
     <View>
-      <Text>Üdv, {user?.username}!</Text>
       <Button title="Kijelentkezés" onPress={handleLogout} />
     </View>
   );
---
+++ frontend/src/app/tabs/character.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function KalandScreen() {
+  return (
+    <View>
+      <Text>Kaland oldal</Text>
+    </View>
+  );
+}
