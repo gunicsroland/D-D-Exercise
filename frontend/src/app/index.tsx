65b50fc117a256fd1657e426162681203712c06b
Roland Gunics <grolcsi09@gmail.com>
2026-03-21 13:38:27 +0100

    refactor(frontend): index.ts uses authContext

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,41 +1,41 @@
 import { useEffect } from "react";
-import { View, Button } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import { checkCharacter } from "../services/character_service";
 import React from 'react';
+import { useAuthContext } from "../context/AuthContext";
+import { colors } from "../styles/colors";
 
 export default function Home() {
     const router = useRouter();
+    const {token, loading} = useAuthContext();
 
     useEffect(() => {
         const checkLogin = async () => {
-            const token = await AsyncStorage.getItem("token");
-            if (!token) {
+            try {
+                if (!token && !loading) {
+                    router.replace("/login");
+                } else if (token){
+                    const hasChar = await checkCharacter(token);
+                    if (!hasChar) {
+                        router.replace("/character/create");
+                    } else {
+                        router.replace("/tabs/character");
+                    }
+                }
+            } catch (error) {
+                console.log("Error checking login:", error);
                 router.replace("/login");
-            }
-            else {
-                const hasChar = await checkCharacter(token);
-                if (!hasChar) {
-                    router.replace("/character/create");
-                }
-                else {
-                    router.replace("/tabs/character");
-                }
             }
         };
 
         checkLogin();
-    }, []);
-
-    const handleLogout = async () => {
-        await AsyncStorage.removeItem("token");
-        router.replace("login");
-    };
+    }, [token, loading]);
 
     return (
-        <View>
-            <Button title="Kijelentkezés" onPress={handleLogout} />
+        <View style={{backgroundColor: colors.background}}>
+            <ActivityIndicator size="large" color={colors.gold}></ActivityIndicator>
         </View>
     );
 }
