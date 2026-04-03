d84e19dedba4c20c4998cb4ec45eb78338226b5f
Roland Gunics <grolcsi09@gmail.com>
2026-04-03 19:31:06 +0200

    test(backend): added better dependency overrides

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from datetime import datetime
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -10,15 +11,16 @@ from sqlalchemy.orm import sessionmaker
 
 from src.main import app
 from src.database import Base, get_db
-from src.dependencies import get_current_user
+from src.dependencies import get_current_user, get_admin_user, get_db
 
-from src.models import User
+from src.models import User, Character, ExerciseDifficulty, ActiveEffect, CharacterClass, AbilityType, CharacterAbility
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
+    pool_size=10, max_overflow=50
 )
 
 TestingSessionLocal = sessionmaker(
@@ -27,6 +29,8 @@ TestingSessionLocal = sessionmaker(
     bind=engine
 )
 
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -35,14 +39,49 @@ def override_get_db():
     finally:
         db.close()
 
-Base.metadata.create_all(bind=engine)
-
 app.dependency_overrides[get_db] = override_get_db
 
 def override_current_user():
-    return User(id=1, username="testuser", email="test@test.com", password_hash="x")
+    user = User(
+        id=1,
+        username="testuser",
+        email="test@test.com",
+        password_hash="x",
+        created_at=datetime.utcnow(),
+        is_admin=False
+    )
+
+    character = Character(
+        id=1,
+        user_id=1,
+        name="Hero",
+        class_=CharacterClass.Barbarian,
+        level=1,
+        xp=0,
+        ability_points=0,
+        quest_difficulty=ExerciseDifficulty.VeryEasy,
+    )
+
+    character.abilities = [
+        CharacterAbility(ability=AbilityType.STR, score=10),
+        CharacterAbility(ability=AbilityType.DEX, score=10),
+        CharacterAbility(ability=AbilityType.CON, score=10),
+        CharacterAbility(ability=AbilityType.INT, score=10),
+        CharacterAbility(ability=AbilityType.WIS, score=10),
+        CharacterAbility(ability=AbilityType.CHA, score=10),
+    ]
+
+    character.active_effects = []
+
+    user.characters = [character]
+
+    return user
 
 app.dependency_overrides[get_current_user] = override_current_user
 
+def override_get_admin_user():
+    return User(id=2, username="adminuser", email="admin@test.com", password_hash="x")
+
+app.dependency_overrides[get_admin_user] = override_get_admin_user
 
 client = TestClient(app)
